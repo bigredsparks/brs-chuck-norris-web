8893f12881ce6cb8b481d29e0ba6156f0baa984b
bigredsparks <svsparks@swbell.net>
2019-02-22 13:54:24 -0600

    Create Spring Boot Project from Spring Initializr.  Closes #1

---
+++ src/main/java/com/bigredsparks/brschucknorrisweb/BrsChuckNorrisWebApplication.java
@@ -0,0 +1,13 @@
+package com.bigredsparks.brschucknorrisweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrsChuckNorrisWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrsChuckNorrisWebApplication.class, args);
+    }
+
+}
